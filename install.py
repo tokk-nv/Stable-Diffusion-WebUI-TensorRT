2025d359d74dfe824a50ae56cb7e279dfbe98e50
Chitoku YATO <cyato@nvidia.com>
2023-10-18 17:47:20 -0700

    Install Polygraphy module

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 import launch
 
 def install():
-    print("installing extension 'stable-diffusion-webui-tensorrt'")
+    print("Installing extension 'stable-diffusion-webui-tensorrt'")
 
     if not launch.is_installed("tensorrt"):
         raise ModuleNotFoundError("could not find tensorrt python module")
@@ -11,6 +11,11 @@ def install():
 
     if not launch.is_installed("onnx"):
         launch.run_pip(f'install onnx', "onnx")
+
+    # Polygraphy	
+    if not launch.is_installed("polygraphy"):
+        print("Polygraphy is not installed! Installing...")
+        launch.run_pip("install polygraphy --extra-index-url https://pypi.ngc.nvidia.com", "polygraphy", live=True)
 
     print("done installing extension 'stable-diffusion-webui-tensorrt'")
  
