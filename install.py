cca764e1e214d47705f6c015b671da60e77e0f0b
Chitoku YATO <cyato@nvidia.com>
2023-10-18 17:36:47 -0700

    Change for Jetson

--- install.py
+++ install.py
@@ -1,31 +1,17 @@
 import launch
-from importlib_metadata import version
 
 def install():
-    if launch.is_installed("tensorrt"):
-        if not version("tensorrt") == "9.0.1.post11.dev4":
-            launch.run(["python","-m","pip","uninstall","-y","tensorrt"], "removing old version of tensorrt")
-        
-    
-    if not launch.is_installed("tensorrt"):
-        print("TensorRT is not installed! Installing...")
-        launch.run_pip("install nvidia-cudnn-cu11==8.9.4.25", "nvidia-cudnn-cu11")
-        launch.run_pip("install --pre --extra-index-url https://pypi.nvidia.com tensorrt==9.0.1.post11.dev4", "tensorrt", live=True)
-        launch.run(["python","-m","pip","uninstall","-y","nvidia-cudnn-cu11"], "removing nvidia-cudnn-cu11")
-        
-    if launch.is_installed("nvidia-cudnn-cu11"):
-        if version("nvidia-cudnn-cu11") == "8.9.4.25":
-            launch.run(["python","-m","pip","uninstall","-y","nvidia-cudnn-cu11"], "removing nvidia-cudnn-cu11")
+    print("installing extension 'stable-diffusion-webui-tensorrt'")
 
-    # Polygraphy	
-    if not launch.is_installed("polygraphy"):
-        print("Polygraphy is not installed! Installing...")
-        launch.run_pip("install polygraphy --extra-index-url https://pypi.ngc.nvidia.com", "polygraphy", live=True)
-    
-    # ONNX GS
-    if not launch.is_installed("onnx_graphsurgeon"):
-        print("GS is not installed! Installing...")
-        launch.run_pip("install protobuf==3.20.2", "protobuf", live=True)
-        launch.run_pip('install onnx-graphsurgeon --extra-index-url https://pypi.ngc.nvidia.com', "onnx-graphsurgeon", live=True)
+    if not launch.is_installed("tensorrt"):
+        raise ModuleNotFoundError("could not find tensorrt python module")
+
+    if not launch.is_installed("pycuda"):
+        launch.run_pip(f'install pycuda', "pycuda")
+
+    if not launch.is_installed("onnx"):
+        launch.run_pip(f'install onnx', "onnx")
+
+    print("done installing extension 'stable-diffusion-webui-tensorrt'")
  
 install()
